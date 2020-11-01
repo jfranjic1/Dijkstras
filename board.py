9c8d2e3b17209af691b8ac939445ac9193976394
jfranjic1 <jfranjic1@etf.unsa.ba>
2020-11-01 15:15:55 +0100

    added start and end node and obstackles

--- board.py
+++ board.py
@@ -1,5 +1,11 @@
 import pygame
 import nodes
+import obstacle
+
+RIGHTCLICK = 1
+LEFTCLICK = 2
+MIDDLECLICK = 3
+NAN = 69
 class Board:
     def __init__(self):
         ########################
@@ -15,7 +21,11 @@ class Board:
         self.surface = pygame.Surface(self.screen.get_size())
         self.surface = self.surface.convert()
         self.startNode = nodes.Node()
+        self.startNodeBool = False
+        self.endNodeBool = False
         self.endNode = nodes.Node()
+        self.obstackles = list()
+        self.obstacklesBool = False
     def start(self):
         temp = pygame.Rect((0,0), (self.WIDTH, self.HEIGHT))
         pygame.draw.rect(self.surface, self.backgroundColor, temp)
@@ -23,11 +33,41 @@ class Board:
             pygame.draw.line(self.surface,self.gridColor, (i, 0), (i, self.HEIGHT))
         for j in range(0, self.HEIGHT , 10):
             pygame.draw.line(self.surface, self.gridColor, (0, j), (self.WIDTH, j))
-        self.startNode.draw(self.surface, 0, 0)
-        self.endNode.draw(self.surface, 790, 590)
-
+        #self.startNode.draw(self.surface, 0, 0)
+        #self.endNode.draw(self.surface, 790, 590)
         while(1):
-            self.clock.tick(10)
+            self.clock.tick(100)
             self.screen.blit(self.surface, (0, 0))
+            self.keypress()
+            if(self.obstacklesBool):
+                self.execute(MIDDLECLICK)
             pygame.display.update()
-            
+
+    def keypress(self):
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        self.execute(RIGHTCLICK)
+                    if event.button == 3:
+                        self.execute(LEFTCLICK)
+                    if event.button == 2:
+                       self.obstacklesBool = True
+            if(event.type == pygame.MOUSEBUTTONUP):
+                 self.obstacklesBool = False
+    def execute(self, command):
+        if(command == RIGHTCLICK):
+            if(self.startNodeBool):
+                self.startNode.delete(self.surface)
+            self.startNode = nodes.Node(1)
+            self.startNode.draw(self.surface, pygame.mouse.get_pos()[0] - pygame.mouse.get_pos()[0]  % self.squareSize, pygame.mouse.get_pos()[1] -  pygame.mouse.get_pos()[1] % self.squareSize)
+            self.startNodeBool = True
+
+        if (command == LEFTCLICK):
+            if (self.endNodeBool):
+                self.endNode.delete(self.surface)
+            self.endNode = nodes.Node(2)
+            self.endNode.draw(self.surface, pygame.mouse.get_pos()[0] - pygame.mouse.get_pos()[0] % self.squareSize, pygame.mouse.get_pos()[1] - pygame.mouse.get_pos()[1] % self.squareSize)
+            self.endNodeBool = True
+        if (command == MIDDLECLICK):
+            self.obstackles.append(obstacle.Obstackle(self.surface, pygame.mouse.get_pos()[0] - pygame.mouse.get_pos()[0] % self.squareSize, pygame.mouse.get_pos()[1] - pygame.mouse.get_pos()[1] % self.squareSize))
+
--- nodes.py
+++ nodes.py
@@ -1,12 +1,18 @@
 import pygame
 
 class Node:
-    def __init__(self):
-        self.squaresize = 10
+    def __init__(self, a = 0):
+        self.squaresize = 8
         self.x = 0
         self.y = 0
-        self.color = (255,0,0)
+        if(a == 0 or a == 1):
+            self.color = (255,0,0)
+        if (a == 2):
+            self.color = (0,255,0)
     def draw(self, surface, x, y):
         self.x = x
         self.y = y
-        pygame.draw.circle(surface, self.color, (self.x + self.squaresize/2, self.y + self.squaresize/2), self.squaresize/2)
+        pygame.draw.circle(surface, self.color, (self.x + self.squaresize/2+1, self.y + self.squaresize/2 + 1), self.squaresize/2)
+
+    def delete(self, surface):
+        pygame.draw.circle(surface, (255,255,255), (self.x + self.squaresize/2+1, self.y + self.squaresize/2 + 1), self.squaresize/2)
