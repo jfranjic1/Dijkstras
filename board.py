94c714afda3df538b141583534488de8970f6d67
jfranjic1 <jfranjic1@etf.unsa.ba>
2020-11-01 13:31:27 +0100

    grid drawn

--- board.py
+++ board.py
@@ -8,10 +8,20 @@ class Board:
         self.WIDTH = 800
         self.HEIGHT  = 600
         self.backgroundColor = (255,255,255)
-        self.gridColor = (0,0,0)
+        self.gridColor = (100,100,100)
         self.squareSize = 10
         self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT), 0, 32)
         self.surface = pygame.Surface(self.screen.get_size())
         self.surface = self.surface.convert()
-    
+    def start(self):
+        temp = pygame.Rect((0,0), (self.WIDTH, self.HEIGHT))
+        pygame.draw.rect(self.surface, self.backgroundColor, temp)
+        for i in range(0, self.WIDTH, 10):
+            pygame.draw.line(self.surface,self.gridColor, (i, 0), (i, self.HEIGHT))
+        for j in range(0, self.HEIGHT , 10):
+            pygame.draw.line(self.surface, self.gridColor, (0, j), (self.WIDTH, j))
+        while(1):
+            self.clock.tick(10)
+            self.screen.blit(self.surface, (0, 0))
+            pygame.display.update()
---
+++ main.py
@@ -0,0 +1,4 @@
+import board
+
+b = board.Board()
+b.start()
